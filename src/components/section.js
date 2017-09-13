63ff5183cfbb0792ca53c670fb97a0c33fea9c2b
Declan Vea <declanpvea@gmail.com>
2017-09-13 11:33:35 -0400

    refactored

--- src/components/section.js
+++ src/components/section.js
@@ -3,41 +3,38 @@ import actors from '../data/actors.js';
 
 export default class Section extends Component {
   render() {
-    let actorsList = actors.map((actor) => {
     return (
-      <section>
-        <img src="https://fillmurray.com/300/300" alt="Bill Murray"/>
-        <li className="list-group-item" key={actor.id}>
-   <ul>
-     <li>
-       <div>Name: {actor.name} </div>
-     </li>
-     <li>
-       <div>Alias: {actor.also_known_as}</div>
-     </li>
-     <li>
-       <div>Popularity (?/10): {actor.popularity}</div>
-     </li>
-     <li>
-       <div>Birthday: {actor.birthday} </div>
-     </li>
+      <div className="list-group-item">
+        <section>
+          <img src="https://fillmurray.com/300/300" alt="Bill Murray"/>
+          {actors.map(actor =>
+            <li className="list-group-item" key={actor.id}>
+            <ul>
+            <li>
+            <div>Name: {actor.name} </div>
+            </li>
+            <li>
+            <div>Alias: {actor.also_known_as}</div>
+            </li>
+            <li>
+            <div>Popularity (?/10): {actor.popularity}</div>
+            </li>
+            <li>
+            <div>Birthday: {actor.birthday} </div>
+            </li>
 
-     <li>
-       <div>Date of Death: {actor.deathday === null ? "Still Alive" :  actor.deathday } </div>
-     </li>
-     <li>
-       <div>Gender: {actor.gender === 1 ? "Female" : "Male" }</div>
-     </li>
-     <p>Biography: {actor.biography}</p>
-   </ul>
- </li>
-      </section>
-    )
-  });
-  return (
-     <div className="list-group-item">
-       {actorsList}
-     </div>
-   );
+            <li>
+            <div>Date of Death: {actor.deathday === null ? "Still Alive" :  actor.deathday } </div>
+            </li>
+            <li>
+            <div>Gender: {actor.gender === 1 ? "Female" : "Male" }</div>
+            </li>
+            <p>Biography: {actor.biography}</p>
+            </ul>
+            </li>
+          )}
+        </section>
+      </div>
+    );
   }
 }
